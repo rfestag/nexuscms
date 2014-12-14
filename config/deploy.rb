b256f508cfeb603a0361b1f1523d770487f65447
Ryan Festag <rfestag@gmail.com>
2014-12-14 16:50:00 -0700

    Updated deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,11 @@
 # config valid only for Capistrano 3.1
-lock '3.2.1'
+lock '3.3.5'
 
 set :application, 'changhon'
 set :deploy_user, 'vpsadmin'
 
 set :scm, :git
 set :repo_url, 'git@github.com:rfestag/nexuscms.git'
-
-set :rbenv_type, :system
-set :rbenv_ruby, '2.1.2'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
